11c66e62f3f2f010830e03a90a2896aa77abf6d6
aengun <aengun@icloud.com>
2021-01-14 16:27:29 +0900

    권한 라이브러리 추가, config

---
+++ src/main/java/com/newlecture/web/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.newlecture.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	
+	
+}
---
+++ src/main/java/com/newlecture/web/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.newlecture.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member/")
+public class MemberController {
+	
+	@RequestMapping("login")
+	public String login() {
+		
+		return "home.member.login";
+	}
+	
+}
